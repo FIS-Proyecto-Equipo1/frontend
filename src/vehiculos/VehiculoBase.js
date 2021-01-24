c5b548caeb9f330008273787deb8dc591a8f9a81
Alfredo Fuentes <afuentes2510@gmail.com>
2021-01-24 13:40:56 +0100

    Vehicle view solved

--- src/vehiculos/VehiculoBase.js
+++ src/vehiculos/VehiculoBase.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function VehiculoBase(props){
     return (
-        <th>
+        <tr>
             <td>{props.vehiculo.matricula}</td>
             <td>{props.vehiculo.estado}</td>
             <td>{props.vehiculo.permiso}</td>
             <td>{props.vehiculo.localizacion}</td>
             <td>{props.vehiculo.tipo}</td>
-            </th>
+            </tr>
     );
 }
 
